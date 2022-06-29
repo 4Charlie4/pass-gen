9467094b69052b25cbe5ce23460626677f89a142
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-06-29 18:27:10 -0400

    starter variables

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,44 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+//Need to have passwordText.value include numbers, text and special characters.
+
   passwordText.value = password;
 
-}
+};
+
+var lowerChar = randomPass(97, 122);
+//var upperChar = randomPass()
+//var specialChar = randomPass()
+//var numbers = randomPass()
+
+ function generatePassword() {
+  lowerChar()
+
+  var passwordTypeUpper = window.confirm("Do you want your password to have numbers in it?");
+  var passwordTypeNumbers = window.confirm("Do you want your password to have numbers in it?");
+  var passwordTypeSpecial = window.confirm("Do you want your password to have special characters in it?");
+   
+  }
+ function randomPass(first, last){
+    var pass = []
+    for (var i = first; i<= last; i++) {
+      pass.push(i);
+      return pass
+    }
+ };
+
+ 
+
+
+
+
+
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
